8ca53d0d1062ec372dd7ff2fb45abc539cc49b62
felix_5bh1a4y <felix_5bh1a4y@FSociety>
2021-11-24 03:06:58 -0800

    yes

---
+++ src/main/java/Clases/ControlDiccionario.java
@@ -0,0 +1,269 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+import javax.swing.JFrame;
+import java.io.BufferedWriter;
+import java.text.Collator;
+import java.util.Locale;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+import org.apache.commons.io.FileUtils;
+
+/**
+ *
+ * @author felix_5bh1a4y
+ */
+public class ControlDiccionario {
+
+    ArrayList<String> diccionario;
+    VentanaOpciones ventanaEmergente;
+    String rutaDiccionario, rutaArchivo,nombreArchivo;
+    Ordenamiento ordenamientos;
+    boolean metodo;
+    HashSet<String> palabrasIgnoradas;
+    int numArchivo;
+
+    public ControlDiccionario(JFrame frame) {
+        ventanaEmergente = new VentanaOpciones(frame, true);
+        ordenamientos = new Ordenamiento();
+        palabrasIgnoradas = new HashSet<>();
+        diccionario = new ArrayList<>();
+        rutaDiccionario = "D:\\Itiel\\5to_Semestre\\Algoritmos y estructuras de datos\\Laboratorio\\Practica7\\src\\main\\java\\Archivos\\diccionario.txt";
+        numArchivo = 0;
+    }
+
+    public void lecturaDiccionario() {
+        try {
+//parsing a CSV file into BufferedReader class constructor  
+
+            BufferedReader br = new BufferedReader(new FileReader(rutaDiccionario));
+            String a = br.readLine();
+            int i = 0;
+            do//returns a Boolean value  
+            {
+                if (i % 10000.0 == 0) {
+                    System.out.println("Leyendo archivo:" + i);
+                }
+                diccionario.add(transformarConAcentos(a));
+                i++;
+            } while ((a = br.readLine()) != null);
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }/*
+        palabrasDiccionario = new String[diccionario.size()];
+        for (int i = 0; i < palabrasDiccionario.length; i++) {
+            palabrasDiccionario[i] = transformarConAcentos(diccionario.get(i)).toLowerCase();
+        }
+        
+        Arrays.sort(palabrasDiccionario);
+        Arrays.sort(palabrasDiccionario);
+        Arrays.sort(palabrasDiccionario);
+        
+            for(int i = 0;i<palabrasDiccionario.length;i++) System.out.println(i + ":" + palabrasDiccionario[i]);*/
+        diccionario.sort(String::compareToIgnoreCase);
+        return;
+    }
+    public void setMetodo(boolean metodo){
+        this.metodo = metodo;
+    }
+
+    public boolean leerArchivos(String ubicacion,String nombreArchivo) {
+        lecturaDiccionario();
+        this.nombreArchivo = nombreArchivo;
+        numArchivo++;
+        rutaArchivo = ubicacion;
+        crearNuevoArchivo(rutaArchivo);
+        ///aqui va el metodo para crear el nuevo archivo
+        File f = new File(rutaArchivo);
+        String[] palabrasDiccionario = new String[diccionario.size()];
+        Scanner s;
+        ArrayList<String> palabrasNuevas = new ArrayList<>();
+        String[] provisional;
+        int i = 0;
+        for (i = 0; i < diccionario.size(); i++) {
+            palabrasDiccionario[i] = diccionario.get(i);
+        }
+        try {
+            s = new Scanner(f);
+            while (s.hasNextLine()) {
+                //Texto sin modificaciones
+                String nueva;
+                String cadenaNormal = transformarConAcentos(s.nextLine());//limpieza de caracteres no alfa numericos
+                String cadena = cadenaNormal.replaceAll("\\p{P}", "");//limpieza de caracteres
+                provisional = cadena.split(" ");//division de la cadena limpia en un vector de strinfs
+                /*for(i =0;i<provisional.length;i++){
+                    provisional[i]= transformarConAcentos(provisional[i]);
+                }*/
+                
+                for (int j = 0; j < provisional.length; j++) {
+                    if (!provisional[j].isEmpty() && !palabrasIgnoradas.contains(provisional[j].toLowerCase()) && !palabrasNuevas.contains(provisional[j].toLowerCase())) {
+                        if (eleccionMetodo(palabrasDiccionario,provisional[j])) {
+                            System.out.println("Encontrada");
+                        } else {
+                            System.out.println("Palabra no encontrada:" + provisional[j].toLowerCase());
+                            desplegarMenu(cadenaNormal, provisional[j]);
+                            switch (ventanaEmergente.getEleccion()) {
+                                case 0://ignorar
+                                    break;
+                                case 1:
+                                    palabrasIgnoradas.add(provisional[j].toLowerCase());//para que no se vuelva a marcar como error
+                                    break;
+                                case 2://a;adir al diccionario
+                                    palabrasNuevas.add(provisional[j].toLowerCase());
+                                    agregarDiccionario(provisional[j].toLowerCase());
+                                    break;
+                                case 3:// reemplazar
+                                    if (ventanaEmergente.getSiCambio()) {
+                                        Sustituir(provisional[j], ventanaEmergente.getNuevaPalabra());
+                                        cadenaNormal=cadenaNormal.replaceAll(provisional[j], ventanaEmergente.getNuevaPalabra());
+                                        palabrasNuevas.add(ventanaEmergente.getNuevaPalabra().toLowerCase());
+                                        agregarDiccionario(ventanaEmergente.getNuevaPalabra().toLowerCase());
+                                    }
+                                    break;
+                                case 4:
+                                    recrearDiccionario();
+                                    System.out.println("Si se recrea");
+                                    return false;
+                            }
+                        }
+                    }
+                }
+                i++;
+            }
+            s.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+    public void desplegarMenu(String cadenaCompleta, String palabra) {
+        ventanaEmergente.enviarTexto(cadenaCompleta, palabra);
+        ventanaEmergente.getEleccion();
+    }
+
+    public String transformarConAcentos(String palabra) {
+        String palabraTransformada = "";
+        byte[] bytes = palabra.getBytes();
+        try {
+            palabraTransformada = new String(bytes, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return palabraTransformada;
+    }
+
+    public void ordenar(ArrayList array) {
+        ArrayList sortedTertiaryCollator = new ArrayList(array);
+        Collator tertiaryCollator = Collator.getInstance(new Locale("es"));
+        tertiaryCollator.setStrength(Collator.TERTIARY);
+        sortedTertiaryCollator.sort(tertiaryCollator);
+        array = sortedTertiaryCollator;
+    }
+
+    public void agregarDiccionario(String palabraNueva) {
+        diccionario.add(palabraNueva);
+    }
+
+    public void escribirEnTxt(ArrayList<String> palabrasNuevas) {
+        try {
+            BufferedWriter bufferWritter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(rutaDiccionario,true), "UTF-8"));
+            BufferedReader br = new BufferedReader(new FileReader("D:\\Itiel\\5to_Semestre\\Algoritmos y estructuras de datos\\Laboratorio\\Practica7\\src\\main\\java\\Archivos\\diccionario.txt"));
+            for (int i = 0; i < palabrasNuevas.size(); i++) {
+                bufferWritter.write("\n" + palabrasNuevas.remove(0));
+            }
+            bufferWritter.close();
+            br.close();
+            lecturaDiccionario();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void recrearDiccionario() {
+        try {
+            BufferedWriter bufferWritter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(rutaDiccionario), "UTF-8"));
+            BufferedReader br = new BufferedReader(new FileReader("D:\\Itiel\\5to_Semestre\\Algoritmos y estructuras de datos\\Laboratorio\\Practica7\\src\\main\\java\\Archivos\\diccionario.txt"));
+            ordenar();
+            for (int i = 0; i < diccionario.size(); i++) {
+                bufferWritter.write(diccionario.get(i)+"\n");
+            }
+            diccionario.sort(String::compareToIgnoreCase);
+
+            bufferWritter.close();
+            br.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void ordenar(){
+    ArrayList sortedTertiaryCollator = new ArrayList(diccionario);
+    Collator tertiaryCollator = Collator.getInstance(new Locale("es"));
+    tertiaryCollator.setStrength(Collator.TERTIARY);
+    sortedTertiaryCollator.sort(tertiaryCollator);
+    diccionario=sortedTertiaryCollator;
+  }
+    
+   private void crearNuevoArchivo(String nombre){
+       String cadena = "";
+       int i = 0;
+       File nuevoArchivo= new File(nombre+"_revisado ("+i+")");
+       while(nuevoArchivo.exists()){
+           i++;
+           cadena = nombre+"_revisado ("+i+")";
+           nuevoArchivo = new File(cadena);
+       }
+       
+       File original = new File(rutaArchivo);
+       try{
+           FileUtils.copyFile(original, nuevoArchivo);
+       } catch (IOException ex) {
+           
+        }
+       System.out.println("Ruta nuevo"+nuevoArchivo.getAbsolutePath());
+       System.out.println("RutaOriginal:"+original.getAbsolutePath());
+       rutaArchivo = nuevoArchivo.getAbsolutePath();
+       System.out.println("Variable Ruta Archivo:"+rutaArchivo);
+       System.out.println("Pokimon");
+       
+   }
+   
+   private boolean eleccionMetodo(String[] palabrasDiccionario,String provisional ){
+       if(metodo){
+           if(ordenamientos.binaria(palabrasDiccionario, provisional.toLowerCase()) != -1){
+               System.out.println("Se uso binario");
+               return true;
+           }else return false;
+       }else{
+           System.out.println("Se uso hash");
+           ordenamientos.Hash(palabrasDiccionario);
+           return ordenamientos.funcionHash(provisional.toLowerCase());
+       }
+   }
+
+    public void Sustituir(String palabraVieja, String palabraNueva) {
+        File textFile = new File(rutaArchivo);
+        try {
+            String data = FileUtils.readFileToString(textFile);
+            data = data.replace(palabraVieja, palabraNueva);
+            FileUtils.writeStringToFile(textFile, data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/Clases/Ordenamiento.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ *
+ * @author felix_5bh1a4y
+ */
+public class Ordenamiento {
+
+    HashMap<Integer, String> elementos;
+
+    public Ordenamiento() {
+        elementos = new HashMap();
+    }
+
+    public static int binaria(String[] a, String x) {
+        int low = 0;
+        int high = a.length - 1;
+        int mid;
+
+        while (low <= high) {
+            mid = (low + high) / 2;
+            if (a[mid].toLowerCase().compareTo(x) < 0) {
+                low = mid + 1;
+            } else if (a[mid].toLowerCase().compareTo(x) > 0) {
+                high = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+
+    public void Hash(String[] diccionario) {
+        int indice = 0;
+        for (int i = 0; i < diccionario.length; i++) {
+            String elemento = diccionario[i];
+            indice = indice + elemento.hashCode();
+            elementos.put(indice, elemento.toLowerCase());
+        }
+    }
+
+    public boolean funcionHash(String x) {
+        if (elementos.containsValue(x)) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+}
